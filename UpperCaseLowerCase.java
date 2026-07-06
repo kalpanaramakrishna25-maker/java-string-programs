826e404e7048254a5cbb770166f62ba0961dc692
Kalpana. M <kalpanaramakrishna25@gmail.com>
2026-07-06 20:22:26 +0530

    Add UpperCaseLowerCase.java to convert case

---
+++ UpperCaseLowerCase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UpperCaseLowerCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+
+        sc.close();
+    }
+}
